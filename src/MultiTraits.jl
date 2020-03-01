1bedd051930e610f45fcd6551269685df234626f
Tom Kwong <tk3369@gmail.com>
2020-03-01 15:04:56 -0800

    Files generated by PkgTemplates

---
+++ src/MultiTraits.jl
@@ -0,0 +1,5 @@
+module MultiTraits
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MultiTraits
+using Test
+
+@testset "MultiTraits.jl" begin
+    # Write your own tests here.
+end
