68af84e50e584852b7ca8a2a224691a43a87fad5
Tom Kwong <tk3369@gmail.com>
2020-03-15 13:31:22 -0700

    args parsing, revamped tests, default not verbose

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,58 +1,123 @@
-# This test is done within a module to ensure proper namespace
-module TestModule
-    using BinaryTraits
-    using Test
-
-    abstract type Ability end
-    @trait Swim as Ability
-    @trait Fly as Ability
-
-    struct Dog end
-    struct Duck end
-
-    @assign Dog with Swim
-    @assign Duck with Swim,Fly
-
-    # basic usage
-    tickle(x) = tickle(flytrait(x), swimtrait(x), x)
-    tickle(::CanFly, ::CanSwim, x) = "Flying high and diving deep"
-    tickle(::CanFly, ::CannotSwim, x) = "Flying away"
-    tickle(::CannotFly, ::CanSwim, x) = "Swam away"
-    tickle(::Ability, ::Ability, x) = "Stuck laughing"
-
-    # composite trait
-    @trait FlySwim as Ability prefix Can,Cannot with Fly,Swim
-    spank(x) = spank(flyswimtrait(x), x)
-    spank(::CanFlySwim, x) = "Flying high and diving deep"
-    spank(::CannotFlySwim, x) = "Too bad"
-
-    function check()
-        @test BinaryTraits.istrait(FlyTrait) === true
-        @test BinaryTraits.istrait(Int) === false
-
-        let dog = Dog(), duck = Duck()
-            @test flytrait(dog) === CannotFly()
-            @test swimtrait(dog) === CanSwim()
-
-            @test flytrait(duck) === CanFly()
-            @test swimtrait(duck) === CanSwim()
-
-            @test tickle(dog) == "Swam away"
-            @test tickle(duck) == "Flying high and diving deep"
-
-            @test spank(dog) == "Too bad"
-            @test spank(duck) == "Flying high and diving deep"
-
-            # Looks like the compiler elide away `flytrait` function if I do this
-            # BinaryTraits.@unassign Duck from Fly
-            # @test spank(duck) == "Too bad"
-        end
-    end
-end # module
-
 using Test
-using .TestModule
+module SingleTrait
+    using BinaryTraits, Test
+    struct Bird end
+    @trait Fly
+    @assign Bird with Fly
 
-@testset "BinaryTraits.jl" begin
-    TestModule.check()
+    function test()
+        @test istrait(FlyTrait)
+        @test supertype(FlyTrait) <: Any
+        @test supertype(CanFly) <: FlyTrait
+        @test supertype(CannotFly) <: FlyTrait
+        @test flytrait(Bird()) == CanFly()
+    end
+end
+
+module MultipleTraits
+    using BinaryTraits, Test
+    struct Duck end
+    struct Dog end
+    @trait Fly
+    @trait Swim
+    @assign Duck with Fly,Swim
+    @assign Dog with Swim
+    function test()
+        @test flytrait(Dog()) == CannotFly()
+        @test swimtrait(Dog()) == CanSwim()
+        @test flytrait(Duck()) == CanFly()
+        @test swimtrait(Duck()) == CanSwim()
+    end
+end
+
+module TraitSuperType
+    using BinaryTraits, Test
+    struct Duck end
+    abstract type Mobility end
+    @trait Fly as Mobility
+    function test()
+        @test supertype(FlyTrait) <: Mobility
+    end
+end
+
+module CustomPrefixes
+    using BinaryTraits, Test
+    @trait Iterable prefix Is,Not
+    @assign AbstractArray with Iterable
+    next(x) = next(iterabletrait(x), x)
+    next(::IsIterable, x) = iterate(x)
+    next(::NotIterable, x) = :toobad
+    function test()
+        @test next([1,2,3]) !== nothing
+        @test next(:hello) === :toobad
+    end
+end
+
+module CompositeTraits
+    using BinaryTraits, Test
+
+    # possible traits
+    @trait Move
+    @trait CarryPassenger
+    @trait FourWheels prefix Has,No
+    @trait Engine prefix Has,No
+
+    # assignments
+    struct Acura end
+    @assign Acura with Move,CarryPassenger,FourWheels,Engine
+    struct Tricycle end
+    @assign Tricycle with Move,CarryPassenger
+
+    # composite
+    @trait Car prefix Is,Not with Move,CarryPassenger,FourWheels,Engine
+
+    function test()
+        @test cartrait(Acura()) == IsCar()
+        @test cartrait(Tricycle()) == NotCar()
+    end
+end
+
+module SyntaxErrors
+    using BinaryTraits, Test
+    using BinaryTraits: SyntaxError
+
+    # This macro expands to testing code that returns true when syntax error
+    # is detected properly
+    macro testme(ex)
+        esc(quote
+                try
+                    @macroexpand($ex)
+                    false
+                catch e
+                    # @info "expansion error" e
+                    e isa LoadError
+                end
+            end)
+    end
+
+    @trait Eat
+    @trait Drink
+    struct Dog end
+
+    function test()
+        @test @testme @trait Fly as                # missing type
+        @test @testme @trait Fly as 1              # 1 is not a type
+        @test @testme @trait Fly prefix            # missing prefix tuple
+        @test @testme @trait Fly prefix 1,2        # wrong type
+        @test @testme @trait Fly prefix Is         # needs two symbols
+        @test @testme @trait Fly prefix Is,Not,Ha  # needs two symbols
+        @test @testme @trait Fly with Eat          # must have at least 2 sub-traits
+        @test @testme @trait Fly with Eat,Drink,1  # 1 is not a symbol
+
+        @test @testme @assign Dog with 1           # 1 is not a symbol
+    end
+end
+
+@testset "My Tests" begin
+    import .SingleTrait;        SingleTrait.test()
+    import .MultipleTraits;     MultipleTraits.test()
+    import .TraitSuperType;     TraitSuperType.test()
+    import .CustomPrefixes;     CustomPrefixes.test()
+    import .CompositeTraits;    CompositeTraits.test()
+    import .SyntaxErrors;       SyntaxErrors.test()
 end
