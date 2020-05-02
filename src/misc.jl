e9b29a10aba95dee304384f72730d98cbd9bccf6
Klaus Crusius <klaus.crusius@web.de>
2020-05-02 12:30:52 +0200

    removed unused code

--- src/misc.jl
+++ src/misc.jl
@@ -11,9 +11,3 @@ function set_verbose(verbose::Bool)
     VERBOSE[] = verbose
 end
 
-# some standard methods which were introduced in v1.2
-if VERSION < v"1.2.0"
-    valtype(::Type{<:AbstractDict{K,V}}) where {K,V} = V
-    valtype(::T) where T<:AbstractDict = valtype(T)
-end
-
